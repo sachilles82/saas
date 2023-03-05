74b388d18bd762009b294481fac6ac0d9e1dcc3d
sachilles <kina98@gmx.ch>
2023-03-05 13:05:50 +0100

    Categorien Controller functions erstellt. Validation wird ausgelagert

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $categories = Category::latest()->paginate();
+
+        return view('categories.index', compact('categories'));
+    }
+
+    public function create(): View
+    {
+        return view('categories.create');
+    }
+
+    public function store(CategoryRequest $request): RedirectResponse
+    {
+        Category::create($request->validated());
+
+        return to_route('categories.index');
+    }
+
+    public function edit(Category $category): View
+    {
+        return view('categories.edit', compact('category'));
+    }
+
+    public function update(CategoryRequest $request, Category $category): RedirectResponse
+    {
+        $category->update($request->validated());
+
+        return to_route('categories.index');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return to_route('categories.index');
+    }
+
 }
