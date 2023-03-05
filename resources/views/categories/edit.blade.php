d248113ee44ff02d98fbc77700f76ca8034c7bcd
sachilles <kina98@gmx.ch>
2023-03-05 15:42:34 +0100

    Category Edit Form korrekt erstellt

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,13 +1,48 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit Category') }}
+            {{ __('Edit category') }}: {{ $category->name }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            khkjjkhj
+            <div>
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-base font-semibold leading-6 text-gray-900">Category</h3>
+                            <p class="mt-1 text-sm text-gray-600">Hier editierst du die Kategorie</p>
+                        </div>
+                    </div>
+                    <div class="mt-5 md:col-span-2 md:mt-0">
+                        <form action="{{ route('categories.update', $category) }}" method="POST">
+                            @csrf
+                            @method('PATCH')
+                            <div class="shadow sm:overflow-hidden sm:rounded-md">
+                                <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+                                    <div class="sm:col-span-6">
+                                        <x-label for="name" :value="__('Name')" />
+                                        <x-input type="text"
+                                                 id="name"
+                                                 name="name"
+                                                 class="block w-full"
+                                                 value="{{ old('name', $category->name) }}"
+                                                 required />
+                                        <x-input-error for="name" class="mt-2" />
+                                    </div>
+                                </div>
+                                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                                    <x-button class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                                        {{ __('Update') }}
+                                    </x-button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
