20bc80f20d180b1396dcd32cd76f996fa532312d
sachilles <kina98@gmx.ch>
2023-03-05 13:13:10 +0100

    Task Controller functions erstellt. Validation wird ausgelagert

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Task;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::with('category', 'user')->latest()->paginate();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function create(): View
+    {
+        $categories = Category::pluck('name', 'id');
+
+        return view('tasks.create', compact('categories'));
+    }
+
+    public function store(TaskRequest $request): RedirectResponse
+    {
+        Task::create($request->validated());
+
+        return to_route('tasks.index');
+    }
+
+    public function edit(Task $task): View
+    {
+        $categories = Category::pluck('name', 'id');
+
+        return view('tasks.edit', compact('task', 'categories'));
+    }
+
+    public function update(TaskRequest $request, Task $task): RedirectResponse
+    {
+        $task->update($request->validated());
+
+        return to_route('tasks.index');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return to_route('tasks.index');
+    }
 }
